4ab2952c9d6476e6f8ec016d145853c9df376fd4
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-05-04 13:15:35 +0300

    some refactors

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,4 @@
-const slugify = require('slugify');
+// const slugify = require('slugify');
 const jwt = require('jsonwebtoken');
 const { v4: uuidv4 } = require('uuid');
 const sharp = require('sharp');
