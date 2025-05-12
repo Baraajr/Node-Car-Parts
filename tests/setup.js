e7bb1b968167d6e4391f4fb485a8601e7144a4e0
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-05-12 20:54:27 +0300

    added setup.js

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,20 @@
-module.exports = {
-  // Other Jest configurations...
-  unhandledRejections: 'strict',
-};
+/* eslint-disable no-undef */
+/* eslint-disable node/no-unpublished-require */
+/* eslint-disable import/no-extraneous-dependencies */
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+let mongod;
+
+beforeAll(async () => {
+  mongod = await MongoMemoryServer.create();
+  const uri = mongod.getUri();
+  await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+  const collections = await mongoose.connection.db.collections();
+  await Promise.all(collections.map((collection) => collection.deleteMany({})));
+  await mongoose.connection.close();
+  await mongod.stop();
+});
