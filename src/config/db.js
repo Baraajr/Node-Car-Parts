6ff0b0ea43befd610ac0522eec658e71dda3a26f
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-05-13 22:51:55 +0300

    some update

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const {
+  USE_LOCAL_DB,
+  USE_SERVICE_DB,
+  USE_ATLAS_DB,
+  DATABASE_LOCAL,
+  DATABASE_SERVICE,
+  DATABASE_ATLAS,
+} = process.env;
+
+let dbUri = '';
+
+if (USE_LOCAL_DB === 'true') dbUri = DATABASE_LOCAL;
+else if (USE_SERVICE_DB === 'true') dbUri = DATABASE_SERVICE;
+else if (USE_ATLAS_DB === 'true') dbUri = DATABASE_ATLAS;
+else throw new Error('No database config selected!');
+
 const connectDb = () =>
   mongoose
-    .connect(process.env.DATABASE)
+    .connect(dbUri)
     .then((conn) =>
       console.log(
         `database connected successfully on host: ${conn.connection.host}`,
